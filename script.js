853e1952a6aae2713056daacfbe54be91d1e26a6
Craig R <102034411+CraigRoyer@users.noreply.github.com>
2024-04-22 00:49:35 -0400

    Update script.js
    Added functionality for unlisted vimeo videos

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-document.querySelectorAll('vimeo-embed, youtube-embed').forEach((v) => {
-  const [poster, src] =
-    v.tagName === 'VIMEO-EMBED'
-      ? [`vumbnail.com/${v.id}.jpg`, 'player.vimeo.com/video']
-      : [`i.ytimg.com/vi/${v.id}/hqdefault.jpg`, 'www.youtube.com/embed'];
+document
+  .querySelectorAll('vimeo-embed, youtube-embed, vimeo-embed-private')
+  .forEach((v) => {
+    const [poster, src] =
+      v.tagName === 'VIMEO-EMBED'
+        ? [`vumbnail.com/${v.id}.jpg`, 'player.vimeo.com/video']
+        : v.tagName === 'YOUTUBE-EMBED'
+        ? [`i.ytimg.com/vi/${v.id}/hqdefault.jpg`, 'www.youtube.com/embed']
+        : [
+            `vumbnail.com/${v.dataset.privateThumb}.jpg`,
+            'player.vimeo.com/video',
+          ];
 
-  v.innerHTML = `<img loading="lazy" src="https://${poster}" alt="${v.title}"><button aria-label="Play"></button>`;
-  v.children[1].addEventListener(
-    'click',
-    () => (
-      (v.innerHTML = `<iframe allow="autoplay" src="https://${src}/${v.id}?autoplay=1"></iframe>`),
-      v.classList.add('active-video')
-    )
-  );
-});
+    v.innerHTML = `<img loading="lazy" src="https://${poster}" alt="${v.title}"><button aria-label="Play"></button>`;
+    v.children[1].addEventListener('click', () => {
+      if (v.tagName === 'VIMEO-EMBED-PRIVATE') {
+        v.innerHTML = `<iframe allow="autoplay" src="https://${src}/${v.id}&autoplay=1"></iframe>`;
+      } else {
+        v.innerHTML = `<iframe allow="autoplay" src="https://${src}/${v.id}?autoplay=1"></iframe>`;
+      }
+      v.classList.add('active-video');
+    });
+  });
 
-const videos = document.querySelectorAll('vimeo-embed, youtube-embed');
+function assignAutoplay(v, src) {
+  if (v.tagName === 'VIMEO-EMBED-PRIVATE') {
+    v.innerHTML = `<iframe allow="autoplay" src="https://${src}/${v.id}&autoplay=1"></iframe>`;
+  } else {
+    v.innerHTML = `<iframe allow="autoplay" src="https://${src}/${v.id}?autoplay=1"></iframe>`;
+  }
+  v.classList.add('active-video');
+}
+
+const videos = document.querySelectorAll(
+  'vimeo-embed, youtube-embed, vimeo-embed-private'
+);
 const close = document.querySelectorAll('.fancybox-close');
 const reel = document.querySelector('.demo-reel-video');
 for (var i = 0; i < close.length; i++) {
@@ -23,17 +42,25 @@ for (var i = 0; i < close.length; i++) {
       const [poster, src] =
         v.tagName === 'VIMEO-EMBED'
           ? [`vumbnail.com/${v.id}.jpg`, 'player.vimeo.com/video']
-          : [`i.ytimg.com/vi/${v.id}/hqdefault.jpg`, 'www.youtube.com/embed'];
+          : v.tagName === 'YOUTUBE-EMBED'
+          ? [`i.ytimg.com/vi/${v.id}/hqdefault.jpg`, 'www.youtube.com/embed']
+          : [
+              `vumbnail.com/${v.dataset.privateThumb}.jpg`,
+              'player.vimeo.com/video',
+            ];
 
       v.innerHTML = `<img loading="lazy" src="https://${poster}" alt="${v.title}"><button aria-label="Play"></button>`;
       v.classList.remove('.active-video');
-      v.children[1].addEventListener(
-        'click',
-        () =>
-          (v.innerHTML = `<iframe allow="autoplay" src="https://${src}/${v.id}?autoplay=1"></iframe>`),
-        v.classList.add('active-video')
-      );
+      v.children[1].addEventListener('click', () => {
+        if (v.tagName === 'VIMEO-EMBED-PRIVATE') {
+          v.innerHTML = `<iframe allow="autoplay" src="https://${src}/${v.id}&autoplay=1"></iframe>`;
+        } else {
+          v.innerHTML = `<iframe allow="autoplay" src="https://${src}/${v.id}?autoplay=1"></iframe>`;
+        }
+        v.classList.add('active-video');
+      });
     });
+
     const source = reel.src;
     reel.src = '';
     reel.src = source;
@@ -48,17 +75,28 @@ for (var i = 0; i < videos.length; i++) {
         const [poster, src] =
           v.tagName === 'VIMEO-EMBED'
             ? [`vumbnail.com/${v.id}.jpg`, 'player.vimeo.com/video']
-            : [`i.ytimg.com/vi/${v.id}/hqdefault.jpg`, 'www.youtube.com/embed'];
+            : v.tagName === 'YOUTUBE-EMBED'
+            ? [`i.ytimg.com/vi/${v.id}/hqdefault.jpg`, 'www.youtube.com/embed']
+            : [
+                `vumbnail.com/${v.dataset.privateThumb}.jpg`,
+                'player.vimeo.com/video',
+              ];
 
         v.innerHTML = `<img loading="lazy" src="https://${poster}" alt="${v.title}"><button aria-label="Play"></button>`;
         v.classList.remove('.active-video');
         v.children[1].addEventListener(
           'click',
-          () =>
-            (v.innerHTML = `<iframe allow="autoplay" src="https://${src}/${v.id}?autoplay=1"></iframe>`),
+          () => {
+            if (v.tagName === 'VIMEO-EMBED-PRIVATE') {
+              v.innerHTML = `<iframe allow="autoplay" src="https://${src}/${v.id}&autoplay=1"></iframe>`;
+            } else {
+              v.innerHTML = `<iframe allow="autoplay" src="https://${src}/${v.id}?autoplay=1"></iframe>`;
+            }
+          },
           v.classList.add('active-video')
         );
       }
     });
   });
 }
+
